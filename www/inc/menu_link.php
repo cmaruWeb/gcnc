e80f43fe74a21c39d054e26c35df7ef0979c8cf9
cmaru <cmaru@cmaru.com>
2025-07-24 15:11:01 +0900

    수정작업

--- www/inc/menu_link.php
+++ www/inc/menu_link.php
@@ -6,7 +6,7 @@
 		$mTitle[0][3][0] = ""; $mLink[0][3][0] = $noSSlDomain."";
 		$mTitle[0][4][0] = ""; $mLink[0][4][0] = $noSSlDomain."";
 		
-	$mTitle[1][0][0] = "탄소중립지원센터"; $mLink[1][0][0] = $noSSlDomain."";
+	$mTitle[1][0][0] = "탄소중립지원센터"; $mLink[1][0][0] = $noSSlDomain."../center/grt.php";
 		$mTitle[1][1][0] = "인사말"; $mLink[1][1][0] = $noSSlDomain."../center/grt.php";
 		$mTitle[1][2][0] = "센터 소개"; $mLink[1][2][0] = $noSSlDomain."../center/overview.php";
 		$mTitle[1][3][0] = "조직도"; $mLink[1][3][0] = $noSSlDomain."../center/org.php";
@@ -14,27 +14,27 @@
 		$mTitle[1][5][0] = "주요사업"; $mLink[1][5][0] = $noSSlDomain."../center/business.php";
 		$mTitle[1][6][0] = "경남기초탄소중립지원센터"; $mLink[1][6][0] = $noSSlDomain."../center/basic_status.php";
 
-	$mTitle[2][0][0] = "탄소중립이란"; $mLink[2][0][0] = $noSSlDomain."";
+	$mTitle[2][0][0] = "탄소중립이란"; $mLink[2][0][0] = $noSSlDomain."../carbon/definition.php";
 		$mTitle[2][1][0] = "탄소중립"; $mLink[2][1][0] = $noSSlDomain."../carbon/definition.php";
 		$mTitle[2][2][0] = "탄소중립 관련 용어 사전"; $mLink[2][2][0] = $noSSlDomain."../carbon/terms.php";
 		$mTitle[2][3][0] = "생활 속 넷제로 참여"; $mLink[2][3][0] = $noSSlDomain."../carbon/netzero.php";
 		$mTitle[2][4][0] = "아이디어 제안"; $mLink[2][4][0] = $noSSlDomain."../carbon/idea.php";
 
-	$mTitle[3][0][0] = "경남의 탄소중립"; $mLink[3][0][0] = $noSSlDomain."";
+	$mTitle[3][0][0] = "경남의 탄소중립"; $mLink[3][0][0] = $noSSlDomain."../gyeongnam/emissions.php";
 		$mTitle[3][1][0] = "온실가스 배출 통계"; $mLink[3][1][0] = $noSSlDomain."../gyeongnam/emissions.php";
 		$mTitle[3][2][0] = "탄소중립·녹색성장 기본계획"; $mLink[3][2][0] = $noSSlDomain."../gyeongnam/plan.php";
 		$mTitle[3][3][0] = "기후위기 적응대책"; $mLink[3][3][0] = $noSSlDomain."../gyeongnam/adaptation.php";
 		$mTitle[3][4][0] = "그래프로 보는 경상남도"; $mLink[3][4][0] = $noSSlDomain."../gyeongnam/graph.php";
 		$mTitle[3][5][0] = "온실가스감축인지 예산제"; $mLink[3][5][0] = $noSSlDomain."../gyeongnam/inventory.php";
 
-	$mTitle[4][0][0] = "탄소중립 소식"; $mLink[4][0][0] = $noSSlDomain."";
+	$mTitle[4][0][0] = "탄소중립 소식"; $mLink[4][0][0] = $noSSlDomain."../news/notice.php";
 		$mTitle[4][1][0] = "공지사항"; $mLink[4][1][0] = $noSSlDomain."../news/notice.php";
 		$mTitle[4][2][0] = "탄소중립 자료"; $mLink[4][2][0] = $noSSlDomain."../news/material.php";
 		$mTitle[4][3][0] = "홍보 자료"; $mLink[4][3][0] = $noSSlDomain."../news/promotion.php";
 		$mTitle[4][4][0] = "보도자료"; $mLink[4][4][0] = $noSSlDomain."../news/press.php";
 
 
-	$mTitle[10][0][0] = "홈페이지안내"; $mLink[10][0][0] = $noSSlDomain."";
+	$mTitle[10][0][0] = "홈페이지안내"; $mLink[10][0][0] = $noSSlDomain."../info/privacy.php";
 		$mTitle[10][1][0] = "개인정보취급방침";     $mLink[10][1][0] = $noSSlDomain."../info/privacy.php";
 		$mTitle[10][2][0] = "이메일무단수집거부";   $mLink[10][2][0] = $noSSlDomain."../info/emailban.php";
 		$mTitle[10][3][0] = "사이트맵";             $mLink[10][3][0] = $noSSlDomain."../info/sitemap.php";
