a07fbe047244fb2ca6aad9aa32a8d07fef0177bc
cmaru <cmaru@cmaru.com>
2025-08-06 13:38:46 +0900

    250806 센터소개 수정

--- www/center/overview.php
+++ www/center/overview.php
@@ -17,71 +17,41 @@
         </div>
 		
         <div class="sub_overview">
-            <div class="titbox bmb">
-                <div class="tit"><p class="h_m">설립배경 및 목적</p></div>
-                <div class="con">
-                    <p class="mb">경상남도 탄소중립지원센터 <b>「기후위기 대응을 위한 탄소중립·녹색성장 기본법」 및 「경상남도 기후위기 대응을 위한 탄소중립·녹색성장 기본 조례」</b>에 근거하여 설립되었습니다.</p>
-                    <ul class="info01">
-                        <li><img src="../img/center/overview_ic0101.png"><h5>탄소중립·녹색성장 정책 실행 지원</h5>
-                            <p>경상남도의 탄소중립 전환과 녹색성장 추진을 위한 계획 수립·이행, 에너지 전환 촉진 등을 담당</p></li>
-                        <li><img src="../img/center/overview_ic0102.png"><h5>지역 맞춤형 정책 확산</h5>
-                            <p>도민·공무원 대상 교육, 홍보, 온실가스 관리, 기후위기 적응대책, 신규정책 발굴, 공모사업 대응 등</p></li>
-                        <li><img src="../img/center/overview_ic0103.png"><h5>지방정부의 실질적 실행력 확보</h5>
-                            <p>경남연구원에 부속된 형태로 운영되며, 2022년 말부터 2024년까지 시범운영 후, 2025년부터 매년 약 4억 원 규모 예산을 지원받아 기능을 강화할 예정</p></li>
-                    </ul>
-                </div>
+            <div class="info01 mb20">
+                <h5>경상남도<span class="col01">탄소중립지원센터</span></h5>
+                <p>Gyeongsangnam-do Carbon Neutrality Support Center</p>
+            </div>
+            <div class="chart bmb">
+                <ul class="dep01 mb20">
+                    <li><img src="../img/center/overview_i0101.jpg"></li>
+                    <li><img src="../img/center/overview_i0102.jpg"></li>
+                </ul>
+                <ul class="dep02 mb20">
+                    <li><p>운영위원회</p></li>
+                </ul>
+                <ul class="dep03 mb20">
+                    <li><img src="../img/logo.png"><p>(재)경상남도환경재단(지정기관)</p></li>
+                </ul>
+                <ul class="dep04">
+                    <li><img src="../img/center/overview_i0201.jpg"><div class="txt"><em>비전</em><p>2050 탄소중립, Net zero 경남</p></div></li>
+                </ul>
             </div>
             <div class="titbox bmb">
-                <div class="tit"><p class="h_m">구체적인 역할</p></div>
+                <div class="tit"><p class="h_m">설립근거</p></div>
                 <div class="con">
-                    <ul class="info01">
-                        <li><h5 class="col01">인식 제고</h5>
-                            <p>도민과 공무원 대상 전문가 강의, 홍보영상, 실천 캠페인 등</p></li>
-                        <li><h5 class="col01">모델 개발 및 확산</h5>
-                            <p>온실가스 감축 모델 마련과 지역 확산 지원</p></li>
-                        <li><h5 class="col01">정부·지자체 사업 분석</h5>
-                            <p>온실가스 감축 영향을 예산단위로 분석</p></li>
-                        <li><h5 class="col01">기후위기 적응</h5>
-                            <p>지방 차원의 대응 전략 마련 및 실행 지원</p></li>
+                    <ul class="box01 dot_li">
+                        <li>기후위기 대응을 위한 탄소중립·녹색성장 기본법 제68조(탄소중립지원센터의 설립),</li>
+                        <li>경상남도 기후위기 대응을 위한 탄소중립·녹색성장 기본 조례 제16조(탄소중립지원센터의 설립·지정·운영 등)</li>
                     </ul>
                 </div>
             </div>
             <div class="titbox">
-                <div class="tit"><p class="h_m">주요사업</p></div>
+                <div class="tit"><p class="h_m">운영목표</p></div>
                 <div class="con">
                     <ul class="info02">
-                        <li><h5><em>01</em>온실가스 관리 및 감축지원</h5>
-                            <dl>
-                                <dt>배출량 모니터링</dt><dd>경남 내 지자체·산업체의 온실가스 배출 현황 실시간 파악</dd>
-                                <dt>감축 모델 개발·확산</dt><dd>지역 특성과 조건에 맞는 감축 전략·기법 개발</dd>
-                                <dt>사업 분석 지원</dt><dd>도 및 시군 차원의 정책ㆍ사업 효과를 예산 단위로 정량 분석</dd>
-                            </dl>
-                        </li>
-                        <li><h5><em>02</em>정책 기획 및 공모사업 대응</h5>
-                            <dl>
-                                <dt>정책 수립 지원</dt><dd>탄소중립·녹색성장 기본법 및 조례에 따른 세부 실행계획 마련</dd>
-                                <dt>공모사업 대응</dt><dd>정부 및 공공기관 공모 기획·제안문 작성 지원</dd>
-                            </dl>
-                        </li>
-                        <li><h5><em>03</em>교육·홍보·역량강화</h5>
-                            <dl>
-                                <dt>도민·공무원 교육</dt><dd>전문가 초빙 강연, 워크숍, 실무형 교육 과정 운영</dd>
-                                <dt>홍보 캠페인</dt><dd>탄소중립 실천을 독려하는 영상, 포스터, SNS 캠페인</dd>
-                                <dt>체험형 프로그램</dt><dd>주민 참여형 온실가스 감축 실천 체험 기회 제공</dd>
-                            </dl>
-                        </li>
-                        <li><h5><em>04</em>기후위기 대응 및 적응</h5>
-                            <dl>
-                                <dt>위험 평가 및 대응계획</dt><dd>폭염·가뭄·해수면 상승 등 기후위기 위협 분석</dd>
-                                <dt>현장 지원</dt><dd>농업, 수산업, 문화재, 도시 등 분야별 맞춤형 대응책 제안</dd>
-                            </dl>
-                        </li>
-                        <li><h5><em>05</em>시범사업 및 네트워크 구축</h5>
-                            <dl>
-                                <dt>지역 시범 프로젝트</dt><dd>마을·학교·공공기관 등에서 온실가스 감축 모델 실증</dd>
-                                <dt>지자체 협업망</dt><dd>시군·유관기관과 협력하여 지역별 네트워크 구축 및 운영</dd>
-                            </dl>
-                        </li>
+                        <li><img src="../img/center/overview_ic0101.png"><p>경상남도 탄소중립 정책연구</p></li>
+                        <li><img src="../img/center/overview_ic0102.png"><p>도정 탄소중립 업무 지원</p></li>
+                        <li><img src="../img/center/overview_ic0103.png"><p>도정 탄소중립 인식 제고 교육 및 홍보</p></li>
                     </ul>
                 </div>
             </div>
